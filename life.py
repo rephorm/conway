b97e192e73c2cb26c8276ecc15a1ff58ad7b3010
Brian Mattern <rephorm@rephorm.com>
2015-02-13 23:07:04 -0800

    Improve comments

--- life.py
+++ life.py
@@ -1,28 +1,31 @@
-""" Conway's Game of Life on an infinite board.  """
+""" Conway's Game of Life on an infinite board.
+
+Throughout the following, points are represented by tuples (i,j).
+"""
 
 import collections
 
 _R = xrange(-1, 2)
-_O = [(i, j) for i in _R for j in _R if i or j]
+_OFFSETS = [(i, j) for i in _R for j in _R if i or j]
 
 
 def move1(x, dx):
-    """ For x = (i, j), dx = (di, dj), returns x + j = (i + di, j + dj). """
+    """ Move x = (i, j) by dx = (di, dj). """
     return tuple(a + b for a, b in zip(x, dx))
 
 
 def move(xs, dx):
-    """ Returns cells that are neighbors of x = (i,j) """
+    """ Returns result of moving each point in xs by dx. """
     return set([move1(x, dx) for x in xs])
 
 
 def neighbors(x):
-    """ Returns cells that are neighbors of x = (i,j) """
-    return move(_O, x)
+    """ Returns cells that are neighbors of x = (i,j). """
+    return move(_OFFSETS, x)
 
 
 def neighbor_counts(living):
-    """ Returns dict of (x,y) => num_neighbors for all cells. """
+    """ Returns dict of pt => num_neighbors for all pts with neighbors. """
     n = collections.Counter()
     for x in map(neighbors, living):
         n.update(x)
@@ -30,12 +33,15 @@ def neighbor_counts(living):
 
 
 def step(living):
-    """ Returns next state. """
+    """ Returns set of living cells after time-stepping. """
     return set([k for k, v in neighbor_counts(living).iteritems()
                 if (k in living and v == 2) or v == 3])
 
+
 if __name__ == "__main__":
-    import shapes, term, time
+    import shapes
+    import term
+    import time
 
     living = set.union(
         move(shapes.hbar(9), (5, 5)),
